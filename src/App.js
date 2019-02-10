37c1cf4380ca890ca026d155a2414e4146342aff
David Mendoza <d.mendoza99@unitec.edu>
2019-02-10 15:28:15 -0600

    listo PWA

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+        <p>
+          Quis ea irure fugiat commodo id duis do consectetur veniam est. Quis reprehenderit laborum
+          id dolore eiusmod duis quis non enim do id labore reprehenderit. Veniam velit nisi sunt
+          reprehenderit est veniam laborum. Amet laborum dolore excepteur fugiat. Culpa nulla
+          reprehenderit consequat id nostrud sunt Lorem minim incididunt. Non ipsum deserunt eiusmod
+          mollit aliquip duis tempor qui ut id ad adipisicing cupidatat. Ut sunt consectetur ullamco
+          nostrud minim minim cupidatat deserunt sit eiusmod. Non minim laborum esse anim voluptate
+          ex nostrud pariatur labore excepteur tempor. Consectetur laboris incididunt amet ut
+          reprehenderit nisi aliqua. Sit officia mollit adipisicing sunt sunt aliquip fugiat
+          voluptate sint duis.
+        </p>
       </div>
     );
   }
