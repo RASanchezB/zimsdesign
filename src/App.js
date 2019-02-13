760817baa8b8813ae60fc95ed246ed77a726dbb8
David Mendoza <d.mendoza99@unitec.edu>
2019-02-13 07:06:11 -0600

    holi

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import * as firebase from "firebase";
 import Home from "./Views/Home";
 import Carrito from "./Views/Carrito";
 import Error404 from "./Views/Error404";
 import Header from "./Components/Header";
 import Productos from "./Views/Productos";
 import "./App.css";
-
+import firebase from "./firebase.js";
 class App extends Component {
   constructor(props) {
     super(props);
-    var config = {
-      apiKey: "AIzaSyCioIfLPdSgZb8MSGZ1T0TQaS-XR6QGrys",
-      authDomain: "zims-design.firebaseapp.com",
-      databaseURL: "https://zims-design.firebaseio.com",
-      projectId: "zims-design",
-      storageBucket: "zims-design.appspot.com",
-      messagingSenderId: "146892583885",
-    };
-    if (!firebase.apps.length) {
-      firebase.initializeApp(config);
-    }
   }
   render() {
     return (
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyCioIfLPdSgZb8MSGZ1T0TQaS-XR6QGrys",
+  authDomain: "zims-design.firebaseapp.com",
+  databaseURL: "https://zims-design.firebaseio.com",
+  projectId: "zims-design",
+  storageBucket: "zims-design.appspot.com",
+  messagingSenderId: "146892583885",
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
