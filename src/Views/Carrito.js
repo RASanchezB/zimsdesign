9ad8e9823c131de5d93df1fd3fed2d44af69be14
David Mendoza <d.mendoza99@unitec.edu>
2019-03-18 09:25:23 -0600

    Por fin :'v

--- src/Views/Carrito.js
+++ src/Views/Carrito.js
@@ -1,19 +1,45 @@
 import React, { Component } from "react";
-import { ListGroup, Jumbotron, Button, Container } from "reactstrap";
+import {
+  ListGroup,
+  Jumbotron,
+  Button,
+  Container,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "reactstrap";
 import { connect } from "react-redux";
 import CartItem from "../Components/CartItem";
 class Carrito extends Component {
   constructor(props) {
     super(props);
-    this.state = { productos: [], total: 0 };
+    this.state = { productos: [], total: 0, modal: false };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal,
+    });
   }
   render() {
     let total = 0;
     Object.keys(this.props.carrito).forEach(element => {
       total += this.props.carrito[element].precio;
     });
+
     return (
       <Container>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>Exito</ModalHeader>
+          <ModalBody>su pedido ha sido hecho exitosamente</ModalBody>
+          <ModalFooter>
+            <Button color="warning" onClick={this.toggle}>
+              OK
+            </Button>
+          </ModalFooter>
+        </Modal>
         {this.props.carrito.length === 0 ? (
           <div style={{ justifyContent: "center", flex: 1 }}>
             <Jumbotron style={{ backgroundColor: "#cdcdcd", textAlign: "center" }}>
@@ -44,15 +70,16 @@ class Carrito extends Component {
             <Button
               color="secondary"
               onClick={() => {
+                let body = { productos: this.props.carrito, total };
                 fetch("https://us-central1-zims-design.cloudfunctions.net/orders/", {
                   method: "post",
-                  body: JSON.stringify(this.props.carrito),
+                  body: JSON.stringify(body),
                 })
                   .then(response => response.json)
                   .then(responsejson => {
-                    console.log(responsejson);
+                    this.toggle();
+                    this.props.clearcart();
                   });
-                this.props.clearcart();
               }}>
               Cancelar
             </Button>
